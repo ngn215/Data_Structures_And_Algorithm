e42cd94f7b38c1adf3d1b12a20eeeaf766c08b05
ngn215 <ngn215@nyu.edu>
2016-09-10 21:14:02 -0400

    Added function to print full BST with structure. still needs more work. Added distance, level, leftOfParent and root parameters to Node class.

--- src/BinarySearchTree.java
+++ src/BinarySearchTree.java
@@ -1,3 +1,6 @@
+import java.util.Queue;
+import java.util.LinkedList;
+
 
 public class BinarySearchTree {
 	
@@ -8,7 +11,7 @@ public class BinarySearchTree {
 		
 		BinarySearchTree bst = new BinarySearchTree();
 		
-		int[] nodesList = {10, 5, 4, 15, 20, 11, 25, 18, 12}; //list of nodes to be inserted to build the tree
+		int[] nodesList = {10, 5, 4, 15, 20, 11, 25, 18, 12, 7}; //list of nodes to be inserted to build the tree
 		Node root = null; //initializing root
 		
 		System.out.println("Constructing tree");
@@ -33,6 +36,19 @@ public class BinarySearchTree {
 		System.out.print("PostOrder Traversal : ");
 		bst.printPostOrderBST(root);
 		
+		int leftMostNodeDistance = bst.calculateLeftMostNodeDistance(root, 0);
+		bst.assignDistanceFromLeft(root, leftMostNodeDistance, 0, 0, "root");
+		
+		System.out.println("");
+		System.out.println("");
+		System.out.println("=== Print Node distances and levels : ===");
+		bst.printNodeDistancesBST(root);
+		
+		System.out.println("");
+		System.out.println("Print BST Structure : ");
+		bst.printBSTStructure(root);
+		
+		
 		System.out.println("");
 		System.out.println("----------------------------------");
 		
@@ -49,7 +65,6 @@ public class BinarySearchTree {
 			bst.printInOrderBST(root);
 			System.out.println("");
 		}
-		
 	}
 
 	public void insertIntoBST(Node root, Node node)
@@ -115,6 +130,143 @@ public class BinarySearchTree {
 		printPostOrderBST(node.left);
 		printPostOrderBST(node.right);
 		System.out.print(node.data + " ");
+	}
+	
+	public void printBSTStructure(Node root)
+	{
+		if (root == null)
+			return;
+		
+		Queue<Node> q = new LinkedList<Node>();
+		q.add(root);
+		
+		while (!q.isEmpty())
+		{			
+			int tempNodeLevel = q.peek().level; 
+			Node temp;
+			
+			Queue<Node> nodesQueue = new LinkedList<Node>();
+			while(!q.isEmpty() && q.peek().level == tempNodeLevel)
+			{
+				temp = q.remove();
+				//System.out.println(temp.data);
+				nodesQueue.add(temp);
+				
+				if(temp.left != null)
+				{
+					q.add(temp.left);
+				}
+				
+				if(temp.right != null)
+				{
+					q.add(temp.right);
+				}
+			}
+			
+			if (!nodesQueue.isEmpty())
+			{
+				treeAtLevelGenerator(nodesQueue);
+				nodesQueue.clear();
+			}
+			
+		}
+		
+	}
+	
+	public String spaceGenerator(int currDistance, int previousDistance)
+	{
+		String spaceToBeAdded = " ";
+		String spaceToBeReturned = "";
+		
+		
+		if(currDistance != 0 && currDistance == previousDistance)
+			return spaceToBeAdded;
+		
+		for(int i=previousDistance; i < currDistance; i++)
+			spaceToBeReturned += spaceToBeAdded;
+		
+		return spaceToBeReturned;
+	}
+	
+	public void treeAtLevelGenerator(Queue<Node> nodesQueue)
+	{
+		//System.out.println("treeAtLevelGenerator");
+		int previousdistanceFromLeft = 0;
+		String nodesString = "";
+		String connectorString = "";
+		
+		while(!nodesQueue.isEmpty())
+		{
+			Node temp = nodesQueue.remove();
+			//System.out.println("treeAtLevelGenerator : " + temp.data);
+			
+			//System.out.println("currDistance : " + temp.distanceFromLeft + " ," + "previousdistanceFromLeft : " + previousdistanceFromLeft);
+			String space = spaceGenerator(temp.distanceFromLeft, previousdistanceFromLeft);
+			//System.out.println("SPACE" + space + "SPACE");
+			
+			if(temp.root)
+			{
+				nodesString += space + temp.data;
+			}
+			else if (temp.leftOfParent)
+			{
+				connectorString += space + "/";
+				nodesString += space + temp.data;
+			}
+			else if (!temp.leftOfParent)
+			{
+				connectorString += space + "\\";
+				nodesString += space + temp.data;
+			}
+			
+			previousdistanceFromLeft = temp.distanceFromLeft;
+		}
+		
+		System.out.println(connectorString);
+		System.out.println(nodesString);
+	}
+	
+	public void assignDistanceFromLeft(Node node, int distanceFromLeft, int distanceFromCenter, int level, String rootOrleftOrRight)
+	{
+		if (node == null)
+			return;
+		
+		if(rootOrleftOrRight.equals("root"))
+			node.root = true;
+		else if(rootOrleftOrRight.equals("left"))
+			node.leftOfParent = true;
+		else if(rootOrleftOrRight.equals("right"))
+			node.leftOfParent = false;
+		
+		assignDistanceFromLeft(node.left, distanceFromLeft - 1, distanceFromCenter - 1, level + 1, "left");
+		
+		node.distanceFromLeft = distanceFromLeft;
+		//node.distanceFromCenter = distanceFromCenter;
+		node.level = level;
+		//System.out.println("Node : " + node.data + ", Distance : " + node.distanceFromLeft);
+		
+		assignDistanceFromLeft(node.right, distanceFromLeft + 1, distanceFromCenter + 1, level + 1, "right");
+	}
+	
+	public void printNodeDistancesBST(Node node)
+	{
+		if (node == null)
+			return;
+		
+		printNodeDistancesBST(node.left);
+		System.out.println("Node : " + node.data 
+							+ ", DistanceFromLeft : " + node.distanceFromLeft 
+							//+ ", DistanceFromCenter : " + node.distanceFromCenter
+							+ ", Level : " + node.level);
+		printNodeDistancesBST(node.right);
+	}
+	
+	public int calculateLeftMostNodeDistance(Node root, int distance)
+	{
+		if(root.left != null)
+			return calculateLeftMostNodeDistance(root.left, distance + 1);
+		
+		return distance;
 	}
 	
 	public Node deleteNodeFromBST(Node root, int data)
@@ -232,29 +384,57 @@ Constructing tree
  \
    12
 
+5
+ \
+   7
+
 ----------------------------------
-InOrder Traversal : 4 5 10 11 12 15 18 20 25 
-PreOrder Traversal : 10 5 4 15 11 12 20 18 25 
-PostOrder Traversal : 4 5 12 11 18 25 20 15 10 
+InOrder Traversal : 4 5 7 10 11 12 15 18 20 25 
+PreOrder Traversal : 10 5 4 7 15 11 12 20 18 25 
+PostOrder Traversal : 4 7 5 12 11 18 25 20 15 10 
+
+=== Print Node distances and levels : ===
+Node : 4, DistanceFromLeft : 0, Level : 2
+Node : 5, DistanceFromLeft : 1, Level : 1
+Node : 7, DistanceFromLeft : 2, Level : 2
+Node : 10, DistanceFromLeft : 2, Level : 0
+Node : 11, DistanceFromLeft : 2, Level : 2
+Node : 12, DistanceFromLeft : 3, Level : 3
+Node : 15, DistanceFromLeft : 3, Level : 1
+Node : 18, DistanceFromLeft : 3, Level : 3
+Node : 20, DistanceFromLeft : 4, Level : 2
+Node : 25, DistanceFromLeft : 5, Level : 3
+
+Print BST Structure : 
+
+  10
+ /  \
+ 5  15
+/  \ /  \
+4  7 11  20
+   \ /  \
+   12 18  25
+
 ----------------------------------
 
 ==== Delete node : 15 ====
 
-InOrder Traversal : 4 5 10 11 12 18 20 25 
+InOrder Traversal : 4 5 7 10 11 12 18 20 25 
 
 ==== Delete node : 18 ====
 
-InOrder Traversal : 4 5 10 11 12 20 25 
+InOrder Traversal : 4 5 7 10 11 12 20 25 
 
 ==== Delete node : 25 ====
 
-InOrder Traversal : 4 5 10 11 12 20 
+InOrder Traversal : 4 5 7 10 11 12 20 
 
 ==== Delete node : 20 ====
 
-InOrder Traversal : 4 5 10 11 12 
+InOrder Traversal : 4 5 7 10 11 12 
 
 ==== Delete node : 12 ====
 
-InOrder Traversal : 4 5 10 11 
+InOrder Traversal : 4 5 7 10 11 
+
  */
--- src/Node.java
+++ src/Node.java
@@ -6,9 +6,16 @@ public class Node {
 	Node next; //used for linked list
 	Node prev; //used for doubly linked list
 	int data;
+	int distanceFromLeft; //will be used to construct BST
+	//int distanceFromCenter; //will be used to construct BST
+	int level; //will be used to construct BST
+	boolean leftOfParent;
+	boolean root;
 	
 	Node(int data)
 	{
 		this.data = data;
+		this.root = false;
+		this.leftOfParent = false;
 	}
 }
